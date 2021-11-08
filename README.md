e5908cd1e821355b0e280320c0e594cee15d8191
Mengbys <1204887512@qq.com>
2021-11-08 20:04:42 +0800

    update readme

--- README.md
+++ README.md
@@ -8,7 +8,56 @@ quicklly. Now it is a hard work for me.
 
 I don't want to waste too much time to configure my Vim/Neovim like before. I
 would like to use the time to coding!
-# Some different plugins install in my Neovim
+
+## Most Used Shortcut
+| shortcut                | function                             |
+| :-:                     | :-:                                  |
+| ,                       | leader key                           |
+| CTRL-n/p                | jump to next/previous buffer         |
+| ,d                      | close current buffer                 |
+| CTRL-j/k/h/l            | window navigation                    |
+| CTRL-down/up/right/left | window moving                        |
+| ,e/q                    | jump to next/previous tabpage        |
+| ,w                      | close current tabpage                |
+| ,,y                     | yank to clipboard                    |
+| ,,ay                    | yank all to clipboard                |
+| ,,p                     | paste clipboard behind               |
+| ,,P                     | paste clipboard front                |
+| ,s                      | save to file                         |
+| ,cd                     | move CWD to current file's directory |
+| ,h                      | open home page                       |
+| ,fm                     | find mru file                        |
+| ,ff                     | find file under CWD                  |
+| ,fb                     | find buffer                          |
+| ,/<char>                | align by <char>                      |
+| ,///                    | align by '//'                        |
+| gc+motion               | comment the field moved              |
+| gcc                     | comment a line                       |
+| gcap                    | comment a block                      |
+| gcu                     | remove comment                       |
+| / or ?                  | enter find mode                      |
+| ;w                      | easymotion in word head              |
+| ;e                      | easymotion in word tail              |
+| ;f                      | easymotion in specific char          |
+| ;j                      | easymotion in line                   |
+| ;;w                     | easymotion forward in word head      |
+| ;;b                     | easymotion backward in word head     |
+| ;;e                     | easymotion forward in word tail      |
+| gd                      | jump to declaration                  |
+| gf                      | jump to definition                   |
+| gi                      | jump to implementation               |
+| [/]d                    | jump to previous/next error          |
+| ALT+b                   | back insert bracket                  |
+| ALT+e                   | put the content behind into brackets |
+| ALT+n                   | jump to next bracket                 |
+| ds<quot>                | delete <quote>                       |
+| ys<textobj><quot>       | add <quote> arround <textobj>        |
+| cs<quot1><quot2>        | change <quot1> to <quot2>            |
+
+## Other Shortcuts
+(under construction)
+
+## Some different plugins installed in my Neovim
 + [symbols-outline.nvim](https://github.com/simrat39/symbols-outline.nvim)
 + [nvim-cmp](https://github.com/hrsh7th/nvim-cmp)
 + [neoscroll.nvim](https://github.com/karb94/neoscroll.nvim)
--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,31 +1,32 @@
 -- normal
 vim.g.mapleader=','
-vim.api.nvim_set_keymap('n','<c-p>',':bp!<cr>',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-n>',':bn!<cr>',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<leader>d',':bd<cr>',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-j>','<c-w>j',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-k>','<c-w>k',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-h>','<c-w>h',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-l>','<c-w>l',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-down>','<c-w>J',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-up>','<c-w>K',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-left>','<c-w>H',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<c-right>','<c-w>L',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<leader>e',':tabn<cr>',{noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<leader>q',':tabp<cr>',{noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<leader>w',':tabc<cr>',{noremap=true,silent=true})
-vim.api.nvim_set_keymap('v','<leader><leader>y','\"+y',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<leader><leader>ay','ggVG\"+y',{noremap=true,silent=true});
-vim.api.nvim_set_keymap('n','<leader><leader>p','\"+p',{noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<leader><leader>P','\"+P',{noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<leader>s',':w<cr>',{noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<leader>cd',':cd %:p:h<cr>',{noremap=true,silent=true})
+
+vim.api.nvim_set_keymap('n' , '<c-p>'              , ':bp!<cr>'      , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-n>'              , ':bn!<cr>'      , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<leader>d'          , ':bd<cr>'       , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-j>'              , '<c-w>j'        , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-k>'              , '<c-w>k'        , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-h>'              , '<c-w>h'        , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-l>'              , '<c-w>l'        , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-down>'           , '<c-w>J'        , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-up>'             , '<c-w>K'        , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-left>'           , '<c-w>H'        , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<c-right>'          , '<c-w>L'        , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<leader>e'          , ':tabn<cr>'     , {noremap=true , silent=true})
+vim.api.nvim_set_keymap('n' , '<leader>q'          , ':tabp<cr>'     , {noremap=true , silent=true})
+vim.api.nvim_set_keymap('n' , '<leader>w'          , ':tabc<cr>'     , {noremap=true , silent=true})
+vim.api.nvim_set_keymap('v' , '<leader><leader>y'  , '\"+y'          , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<leader><leader>ay' , 'ggVG\"+y'      , {noremap=true , silent=true});
+vim.api.nvim_set_keymap('n' , '<leader><leader>p'  , '\"+p'          , {noremap=true , silent=true})
+vim.api.nvim_set_keymap('n' , '<leader><leader>P'  , '\"+P'          , {noremap=true , silent=true})
+vim.api.nvim_set_keymap('n' , '<leader>s'          , ':w<cr>'        , {noremap=true , silent=true})
+vim.api.nvim_set_keymap('n' , '<leader>cd'         , ':cd %:p:h<cr>' , {noremap=true , silent=true})
 
 
 -- dashboard
-vim.api.nvim_set_keymap('n','<leader>h',':Dashboard<cr>',{noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<leader>fm',':DashboardFindHistory<CR>',{noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<leader>ff',':DashboardFindFile<CR>',{noremap=true,silent=true})
+vim.api.nvim_set_keymap('n' , '<leader>h'  , ':Dashboard<cr>'            , {noremap=true , silent=true})
+vim.api.nvim_set_keymap('n' , '<leader>fm' , ':DashboardFindHistory<CR>' , {noremap=true , silent=true})
+vim.api.nvim_set_keymap('n' , '<leader>ff' , ':DashboardFindFile<CR>'    , {noremap=true , silent=true})
 
 
 -- fzf
