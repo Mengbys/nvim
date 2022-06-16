41bef8a6853bf24632f368c031e44f291a864840
Mengbys <1204887512@qq.com>
2022-06-16 20:38:33 +0800

    2022-06-16

--- init.lua
+++ init.lua
@@ -7,7 +7,6 @@ require('settings')
 require('keymappings')
 require('functions')
 require('colorscheme')
--- require('plugin_airline')
 require('plugin_lualine')
 require('plugin_bufferline')
 require('plugin_lspconfig')
@@ -20,10 +19,9 @@ require('plugin_neoscroll')
 require('plugin_dashboard')
 require('plugin_tabular')
 require('plugin_goyo')
-require('plugin_fzf_vim')
 -- require('plugin_fzf_lua')
+require('plugin_fzf_vim')
 require('plugin_fterm')
--- require('plugin_alpha')
 
 -- re: setting
 vim.o.laststatus = 3
@@ -35,12 +33,6 @@ vim.g.AutoPairsFlyMode=1
 -- clever-f
 vim.g.clever_f_across_no_line=1
 
--- ultisnips
--- vim.g.UltiSnipsExpandTrigger       = "<c-x>"
--- vim.g.UltiSnipsJumpForwardTrigger  = "<c-b>"
--- vim.g.UltiSnipsJumpBackwardTrigger = "<c-z>"
--- vim.g.UltiSnipsSnippetDirectories  = {"UltiSnips", "mycoolsnippets"}
-
 -- vsnip
 vim.g.vsnip_snippet_dir = 'C:/Users/12048/AppData/Local/nvim/vsnip'
 
@@ -48,12 +40,7 @@ vim.g.vsnip_snippet_dir = 'C:/Users/12048/AppData/Local/nvim/vsnip'
 vim.g.glow_binary_path='D:/glow/'
 vim.api.nvim_set_keymap('n','gl',':Glow<CR>',{noremap=true,silent=true})
 
--- vim-xkbswitch
-vim.g.XkbSwitchEnabled = 1
-vim.g.XkbSwitchLib = 'D:/Neovim/bin/libxkbswitch64.dll'
-vim.g.XkbSwitchNLayout = 'us'
-vim.g.XkbSwitchILayout = 'cn'
-vim.g["airline#extensions#xkblayout#enabled"] = 0
-
 -- markdown preview
--- vim.g.mkdp_browser = 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'
+vim.g.mkdp_browser = 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'
+vim.g.mkpd_port = '20000'
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,6 @@
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', 'C:/Users/12048/AppData/Local/nvim/plugged')
 
--- new
 -- plugins using lua
 Plug 'kyazdani42/nvim-web-devicons'
 Plug 'kyazdani42/nvim-tree.lua'
@@ -13,24 +12,17 @@ Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/nvim-cmp'
--- Plug 'quangnguyen30192/cmp-nvim-ultisnips'
 Plug 'hrsh7th/cmp-vsnip'
 Plug 'hrsh7th/vim-vsnip'
--- Plug 'hrsh7th/cmp-nvim-lua'
--- Plug 'hrsh7th/cmp-omni'
 Plug 'rafamadriz/friendly-snippets'
 Plug 'karb94/neoscroll.nvim'
 Plug('nvim-treesitter/nvim-treesitter', {['do']=vim.fn[':TSUpdate']})
 Plug 'ellisonleao/glow.nvim'
 Plug 'numToStr/FTerm.nvim'
--- Plug 'akinsho/toggleterm.nvim'
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'akinsho/bufferline.nvim'
 Plug 'lewis6991/impatient.nvim'
--- Plug 'goolord/alpha-nvim'
 
-
--- new find engine
 Plug('junegunn/fzf', { ['do'] = vim.fn['fzf#install'] })
 Plug 'junegunn/fzf.vim'
 -- Plug 'ibhagwan/fzf-lua'
@@ -42,15 +34,9 @@ Plug 'overcache/NeoSolarized'
 Plug 'pineapplegiant/spaceduck'
 Plug 'liuchengxu/space-vim-dark'
 Plug 'folke/tokyonight.nvim'
--- Plug 'vim-airline/vim-airline'
--- Plug 'vim-airline/vim-airline-themes'
--- Plug 'ryanoasis/vim-devicons'
--- Plug 'preservim/nerdtree'
--- Plug 'tiagofumo/vim-nerdtree-syntax-highlight'
+Plug 'morhetz/gruvbox'
 Plug 'jiangmiao/auto-pairs'
 Plug 'easymotion/vim-easymotion'
--- Plug 'SirVer/ultisnips'
--- Plug 'mengbys/vim-snippets'
 Plug 'godlygeek/tabular'
 Plug 'tpope/vim-commentary'
 Plug 'tpope/vim-surround'
@@ -62,8 +48,5 @@ Plug 'jeffkreeftmeijer/vim-numbertoggle'
 Plug 'junegunn/goyo.vim'
 Plug 'dstein64/vim-startuptime'
 -- Plug 'gcmt/taboo.vim'
--- Plug 'sheerun/vim-polyglot'
 Plug('iamcco/markdown-preview.nvim', { ['do'] = vim.fn['mkdp#util#install'], ['for'] = {'markdown', 'vim-plug'} })
-Plug 'lyokha/vim-xkbswitch'
--- Plug 'brglng/vim-im-select'
 vim.call('plug#end')
