5096340004f37777c581b15ea84103f61e3c2903
mengbys <1204887512@qq.com>
2024-05-22 13:48:49 +0800

    plugin path, svnip path, config path

--- init.lua
+++ init.lua
@@ -1,13 +1,8 @@
 Plugins_dir=""
 Snips_dir=""
 
-if (vim.loop.os_uname().sysname == 'Windows_NT') then
-  Plugins_dir=vim.fn.fnamemodify("D:/Neovim/lazy/", ":p")
-  Snips_dir=vim.fn.fnamemodify("D:/Neovim/vsnip/", ":p")
-else
-  Plugins_dir=vim.fn.fnamemodify("~/.config/nvim/lazy/", ":p")
-  Snips_dir=vim.fn.fnamemodify("~/.config/nvim/vsnip/", ":p")
-end
+Plugins_dir=vim.fn.stdpath("config").."/lazy"
+Snips_dir=vim.fn.stdpath("config").."/vsnip"
 
 local lazypath=Plugins_dir .. "/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
